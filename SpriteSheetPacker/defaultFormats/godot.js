25e7019bc4591e86a4586e727699ec4becbc0e3a
piratesephiroth <piratesephiroth@users.noreply.github.com>
2019-03-03 14:59:10 -0300

    add Godot AnimatedSprite as publish format
    You have to point out where the texture is located when the scene is imported into Godot.
    (I think the script can't read files on disk so we can't determine where project.godot is to add the correct texture location to the file.)

---
+++ SpriteSheetPacker/defaultFormats/godot.js
@@ -0,0 +1,58 @@
+
+
+function exportSpriteSheet(dataFilePath, imageFilePath, spriteFrames)
+{
+    var loopCount = 0;
+    var contents = "";
+    var imageList = "";
+    var frameList = "";
+    var imageCount = Object.keys(spriteFrames).length;
+    var spriteFrame;
+  
+    for (var key in spriteFrames)
+    {
+        spriteFrame = spriteFrames[key];
+        imageList += "[sub_resource type=\"AtlasTexture\" id=" + (loopCount + 1) + "]\n"
+        imageList += "atlas = ExtResource( 1 )\n"
+        imageList += "region = Rect2( " + spriteFrame.frame.x + ", " +
+                                          spriteFrame.frame.y + ", " +
+                                          spriteFrame.frame.width + ", " +
+                                          spriteFrame.frame.height + ")\n"
+        imageList += "margin = Rect2( " + spriteFrame.sourceColorRect.x + ", " +
+                                          spriteFrame.sourceColorRect.y + ", " +
+                                          (spriteFrame.sourceSize.width - spriteFrame.frame.width) + ", " +
+                                          (spriteFrame.sourceSize.height - spriteFrame.frame.height) + " )\n"
+        imageList += "\n"
+        loopCount++
+        frameList += "SubResource( " + loopCount + " )";
+        
+        if(loopCount < imageCount)
+        {
+            frameList += ", ";
+        }
+    }
+    
+    contents += "[gd_scene load_steps=" + (imageCount + 3) + " format=2]\n";
+    contents += "\n";
+    contents += "[ext_resource path=\"res://" + imageFilePath.replace(/^.*[\\\/]/, '') + "\" type=\"Texture\" id=1]\n"
+    contents += "\n";
+    contents += imageList;
+    contents += "[sub_resource type=\"SpriteFrames\" id=" + (imageCount + 1) + "]\n"
+    contents += "animations = [ {\n"
+    contents += "\"frames\": [ " + frameList + " ],\n"
+    contents += "\"loop\": false,\n"
+    contents += "\"name\": \"default\",\n"
+    contents += "\"speed\": 5.0\n"
+    contents += "} ]\n"
+    contents += "\n";
+    contents += "[node name=\"AnimatedSprite\" type=\"AnimatedSprite\"]\n";
+    contents += "frames = SubResource( " + (imageCount + 1) + " )\n";
+    contents += "animation = \"default\"\n"
+    contents += "frame = 1\n"
+    contents += "\n";
+    
+    return {
+        data: contents,
+        format: "tscn"
+    };
+}
