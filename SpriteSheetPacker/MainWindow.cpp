f87a74a3d7c5ad8edf3f49b7268a577b25ad70b7
piratesephiroth <piratesephiroth@users.noreply.github.com>
2021-02-24 10:02:13 -0300

    save path to destination folder

--- SpriteSheetPacker/MainWindow.cpp
+++ SpriteSheetPacker/MainWindow.cpp
@@ -811,13 +811,15 @@ void MainWindow::spritesTreeWidgetItemSelectionChanged() {
 }
 
 void MainWindow::on_destFolderToolButton_clicked() {
+    QSettings settings;
     QString destPath = ui->destPathLineEdit->text();
     destPath = QFileDialog::getExistingDirectory(this,
                                                  tr("Destination folder"),
-                                                 destPath,
+                                                 settings.value("MainWindow/destinationFolder", QDir::currentPath()).toString(),//destPath,
                                                  QFileDialog::DontResolveSymlinks);
     if (!destPath.isEmpty()) {
         ui->destPathLineEdit->setText(QDir(destPath).canonicalPath());
+        settings.setValue("MainWindow/destinationFolder", destPath);
     }
 }
 
